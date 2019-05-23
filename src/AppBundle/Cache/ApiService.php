8fcfba9b1a91966a2e2b6193cfb95bbd1cdb7326
Ryan W. Howe <ryanwhowe@gmail.com>
2019-05-23 14:24:53 -0400

    Removed the cache control, there is no cron service on the docker container

--- src/AppBundle/Cache/ApiService.php
+++ src/AppBundle/Cache/ApiService.php
@@ -34,6 +34,11 @@ class ApiService
     CONST TOKEN_TYPE_DATA = 2;
 
     /**
+     * @var int the time in seconds that the cache should be invalidated
+     */
+    private $cache_timeout = 300;
+
+    /**
      * @var LoggerInterface
      */
     private $logger;
@@ -46,6 +51,21 @@ class ApiService
     private function __construct(LoggerInterface $logger)
     {
         $this->logger = $logger;
+    }
+
+
+    /**
+     * The internal cache needs to be renewed every half hour since this is when the
+     * machines will updated there status'.  This will calculated when the last half
+     * hour passed for comparing if the cache should still be used or updated
+     *
+     * @return int
+     */
+    private function getCacheTimeout()
+    {
+        //Find how far off the prior interval we are
+        //Removing this offset takes us to the "round down" half hour
+        return time() - (time() % $this->cache_timeout);
     }
 
     /**
@@ -85,7 +105,7 @@ class ApiService
 
         $has_cache = $cache->has($time_token) && $cache->has($data_token);
 
-        if ($has_cache && $cache->get($time_token)) {
+        if ($has_cache && $cache->get($time_token) >= $this->getCacheTimeout()) {
             $result = $cache->get($data_token);
             $this->logger->info('Cache result', $result);
             $this->logger->info('Cache Time', ['timestamp' => $cache->get($time_token)]);
@@ -99,37 +119,8 @@ class ApiService
         return $result;
     }
 
-    /**
-     * @param string $grouping
-     * @param string $api_url
-     * @param string $api_token
-     *
-     * @return array
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     * @throws \Exception
-     */
-    private function getLiveServerGroup(string $grouping, string $api_url, string $api_token){
-        $cache = new FilesystemCache();
-
-        $time_token = $this->generateCacheToken(self::TOKEN_TYPE_TIME, $grouping);
-        $data_token = $this->generateCacheToken(self::TOKEN_TYPE_DATA, $grouping);
-
-        $result = Client::create($api_token, $api_url, $grouping)->group();
-        $this->logger->info('Api result', $result);
-        $cache->set($data_token, $result);
-        $cache->set($time_token, time());
-
-        return $result;
-
-    }
-
     static public function getServerGroupData(string $grouping, string $api_url, string $api_token, LoggerInterface $logger){
         $self = new self($logger);
         return $self->getServerGroup($grouping, $api_url, $api_token);
-    }
-
-    static public function updateServerCacheData(string $grouping, string $api_url, string $api_token, LoggerInterface $logger){
-        $self = new self($logger);
-        return $self->getLiveServerGroup($grouping, $api_url, $api_token);
     }
 }
