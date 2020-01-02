d9ebedea3724f74c3f044d888922db7874f95c4b
Ryan Howe <rhowe@truecar.com>
2020-01-02 13:15:03 -0500

    Issue #37 : Add today's weather widget * corrected time check bug * added better logging for debugging

--- src/Statusboard/Weather/Accuweather/Cache.php
+++ src/Statusboard/Weather/Accuweather/Cache.php
@@ -80,9 +80,13 @@ class Cache
         $token = $this->generateCacheToken(self::TOKEN_TYPE_TIME, $cacheType);
 
         if($this->cache->has($token)){
-            $this->logger->info('Has ' . $token . ' token', [$this->cache->get($token)]);
-            return time() >= $this->cache->get($token);
+            $this->logger->info('Has ' . $token . ' token', [
+                'time token' => $this->cache->get($token),
+                'current time' => time()
+            ]);
+            return time() <= $this->cache->get($token);
         } else {
+            $this->logger->info('There was no cached value', []);
             return false;
         }
     }
@@ -94,9 +98,10 @@ class Cache
      * @throws \Psr\SimpleCache\InvalidArgumentException
      */
     public function updateCache(string $cacheType, int $timeout, string $value){
-        $this->logger->info('Updated '. $cacheType,[]);
-        $this->logger->info('timeout', [$timeout]);
-        $this->logger->info('value', [$value]);
+        $this->logger->info('Updated '. $cacheType,[
+            'timeout'=> $timeout,
+            'value' => $value
+        ]);
         $this->cache->set($this->generateCacheToken(self::TOKEN_TYPE_TIME, $cacheType), $timeout + (60 * 60));
         $this->cache->set($this->generateCacheToken(self::TOKEN_TYPE_DATA, $cacheType), $value);
     }
@@ -107,7 +112,9 @@ class Cache
      * @throws \Psr\SimpleCache\InvalidArgumentException
      */
     public function getCache(string $cacheType){
-        return $this->cache->get($this->generateCacheToken(self::TOKEN_TYPE_DATA, $cacheType));
+        $cache = $this->cache->get($this->generateCacheToken(self::TOKEN_TYPE_DATA, $cacheType));
+        $this->logger->info('cached value', [$cache]);
+        return $cache;
     }
 
 }
