85e0759a5eeae8d4128a6d8fa50a2a2ae8dbeeaf
Ryan Howe <rhowe@truecar.com>
2020-03-16 17:08:41 -0400

    General_Improvements * upgraded packages in composer * used dependency injection for logger to remove deprecation warnings from phpunit * Added icons to main page containers * Updated TRUECar to TrueCar as part of rebranding with company and all test effected

--- tests/AppBundle/Entity/CalendarTest.php
+++ tests/AppBundle/Entity/CalendarTest.php
@@ -11,12 +11,12 @@ class CalendarTest extends TestCase {
     public function translateTypeDescriptionDataProvider(){
         return [
             /* input, expected */
-            [new Calendar(['type' => Calendar::TYPE_COMPANY_HOLIDAY, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2019-11-28')]), 'TRUECar Holiday'],
+            [new Calendar(['type' => Calendar::TYPE_COMPANY_HOLIDAY, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2019-11-28')]), 'TrueCar Holiday'],
             [new Calendar(['type' => Calendar::TYPE_COMPANY_HOLIDAY, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2015-11-28')]), 'Ives Holiday'],
             [new Calendar(['type' => Calendar::TYPE_COMPANY_HOLIDAY, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2012-11-28')]), 'Avention Holiday'],
             [new Calendar(['type' => Calendar::TYPE_COMPANY_HOLIDAY, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2006-11-28')]), 'JNJ Holiday'],
 
-            [new Calendar(['type' => Calendar::TYPE_PAY_DATE, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2019-11-28')]), 'TRUECar Pay Date'],
+            [new Calendar(['type' => Calendar::TYPE_PAY_DATE, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2019-11-28')]), 'TrueCar Pay Date'],
             [new Calendar(['type' => Calendar::TYPE_PAY_DATE, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2015-11-28')]), 'Ives Pay Date'],
             [new Calendar(['type' => Calendar::TYPE_PAY_DATE, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2012-11-28')]), 'Avention Pay Date'],
             [new Calendar(['type' => Calendar::TYPE_PAY_DATE, 'eventDate' => DateTime::createFromFormat('Y-m-d', '2006-11-28')]), 'JNJ Pay Date'],
