528e5bdc0ba44bb23f5fb27e49476cd118fcf2d0
Ryan Howe <ryanwhowe@gmail.com>
2020-04-24 09:55:07 -0400

    MBTA route name change hotfix * the MBTA api changed the way it named the routes causing it to no longer be a reliable filter * added a new TripFilters for filtering trips on headsigns which has a direction implicitly included in it

--- src/Statusboard/Mbta/TripFilters.php
+++ src/Statusboard/Mbta/TripFilters.php
@@ -9,6 +9,8 @@ class TripFilters {
     const TRIP_OUTBOUND = 0;
     const TRIP_INBOUND = 1;
 
+    const HEADSIGN_FORGEPARK = "Forge Park/495";
+
     public static function tripDirectionFilter($trip_direction){
         if(!self::validateTripDirection($trip_direction)) throw new \InvalidArgumentException('Invalid trip direction \'' . $trip_direction . '\'');
         return function($trip) use ($trip_direction) {
@@ -19,9 +21,21 @@ class TripFilters {
         };
     }
 
+    /**
+     * @deprecated The route patter has changed and is no longer a reliable filter
+     */
     public static function routePatternFilter($route_pattern){
         return function ($trip) use ($route_pattern) {
             if ($trip['relationships']['route_pattern']['data']['id'] === $route_pattern) {
+                return true;
+            }
+            return false;
+        };
+    }
+
+    public static function headSignFilter($headsign){
+        return function ($trip) use ($headsign) {
+            if ($trip['attributes']['headsign'] === $headsign) {
                 return true;
             }
             return false;
