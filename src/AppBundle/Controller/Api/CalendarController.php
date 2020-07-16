67b383cc3af023ee673e0390a0a878fd71022c64
Ryan W. Howe <ryanwhowe@gmail.com>
2020-07-16 09:51:01 -0400

    Implement Upcoming Endpoint #83

---
+++ src/AppBundle/Controller/Api/CalendarController.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+
+namespace AppBundle\Controller\Api;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class CalendarController
+ * @Route("/api/calendar")
+ *
+ * @package AppBundle\Controller\Api
+ */
+class CalendarController extends Controller {
+
+}
