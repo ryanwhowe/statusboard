ad548eaa46ea9e531b6a1604bcf7d227201a3edd
Ryan W. Howe <ryanwhowe@gmail.com>
2021-01-02 13:33:14 -0500

    trying to encapsulate the access control addition to the response

--- src/AppBundle/Controller/Api/ApiController.php
+++ src/AppBundle/Controller/Api/ApiController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class ApiController extends Controller {
 
@@ -24,5 +25,8 @@ abstract class ApiController extends Controller {
         return new ParameterBag(is_array($array) ? $array : []);
     }
 
+    protected function returnResponse(Response $response) {
+        return $response->headers->add(["Access-Control-Allow-Origin" => "*"]);
+    }
 
 }
